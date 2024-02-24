eeaa444f687b3709eda8d54401eeee93ee6d8af3
alec-grace <agrace072@protonmail.com>
2024-02-23 22:20:10 -0500

    Altering approach to align with intended method

--- embeddings.py
+++ embeddings.py
@@ -5,79 +5,56 @@
 #   Create word embeddings from a given dataset
 
 import nltk
+import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 import gensim
+import gensim.downloader as api
 from gensim.models import Word2Vec
+import pickle
+import os
 
 
+# def training(train_file: str) -> list:
+#     corpus = ''
+#     with open(train_file, 'r') as infile:
+#         lines = infile.readlines()
+#     infile.close()
+#
+#     for line in lines:
+#         corpus = corpus + line.replace("\n", " ")
+#
+#     data = []
+#     sents = sent_tokenize(corpus)
+#     for sent in sents:
+#         temp = []
+#         for word in word_tokenize(sent):
+#             temp.append(word)
+#         data.append(temp)
+#     return data
+#
+#
+# def text2vec(corpus):
+#     mod_min = 1
+#     vec_size = 300
+#     window = 10
+#     sg = 0
+#     model = Word2Vec.load("archive/GoogleNews-vectors-negative300.bin")
+
+
+# paperswithcode tend to have datasets with research papers
 def main():
+    # model = Word2Vec.load("GoogleNews-vectors-negative300.bin")
+    # print(model.wv['hello'])
+    if os.path.exists("goog_news.bin"):
+        wv = open("goog_news.bin", "wb").readlines()
+    else:
+        wv = api.load('word2vec-google-news-300')
+        with open("goog_news.bin", 'wb') as outfile:
+            pickle.dump(wv, outfile)
+        outfile.close()
 
-    with open("test_corpus.txt", 'r') as infile:
-        lines = infile.readlines()
-    infile.close()
-    corpus = ''
-    for line in lines:
-        corpus = corpus + line
-    corpus.replace("\n", " ")
-    corpus.replace("*", " ")
-    sents = sent_tokenize(corpus)
-    data = []
-    for sent in sents:
-        temp = []
-        for word in word_tokenize(sent):
-            temp.append(word)
-        data.append(temp)
-
-    with open("test_corpus2.txt", 'r') as infile:
-        lines = infile.readlines()
-    infile.close()
-
-    corpus2 = ''
-    for line in lines:
-        corpus2 = corpus2 + line.replace("\n", " ")
-    corpus2.replace("\n", " ")
-    corpus2.replace("*", " ")
-    sents2 = sent_tokenize(corpus2)
-    data2 = []
-    for sent in sents2:
-        temp = []
-        for word in word_tokenize(sent):
-            temp.append(word)
-        data2.append(temp)
-
-    test_word1 = "heart"
-    test_word2 = "organ"
-    test_word3 = "guts"
-    test_word4 = "OpenAI"
-    test_word5 = "tech"
-
-    mod_min = 1
-    vec_size = 5
-    window = 5
-    sg = 1
-    model = gensim.models.Word2Vec(data, min_count=mod_min, vector_size=vec_size, window=window)
-    model2 = gensim.models.Word2Vec(data, min_count=mod_min, vector_size=vec_size, window=window, sg=sg)
-
-    model3 = gensim.models.Word2Vec(data2, min_count=mod_min, vector_size=vec_size, window=window)
-    model4 = gensim.models.Word2Vec(data2, min_count=mod_min, vector_size=vec_size, window=window, sg=sg)
-
-    with open('test_results.txt', 'a') as outfile:
-        outfile.write("\n\nmin_count = " + str(mod_min) + "\nvector_size = " + str(vec_size) +
-                      "\nwindow = " + str(window) + "\nskip gram = " + str(sg))
-        outfile.write("\nbow similarity between " + test_word1 + " and " + test_word2 +
-                      ": " + str(model.wv.similarity(test_word1, test_word2)))
-        outfile.write("\nsg similarity between " + test_word1 + " and " + test_word2 +
-                      ": " + str(model2.wv.similarity(test_word1, test_word2)))
-        outfile.write("\nbow similarity between " + test_word1 + " and " + test_word3 +
-                      ": " + str(model.wv.similarity(test_word1, test_word3)))
-        outfile.write("\nsg similarity between " + test_word1 + " and " + test_word3 +
-                      ": " + str(model2.wv.similarity(test_word1, test_word3)))
-        outfile.write("\nbow similarity between " + test_word4 + " and " + test_word5 +
-                      ": " + str(model3.wv.similarity(test_word4, test_word5)))
-        outfile.write("\nsg similarity between " + test_word4 + " and " + test_word5 +
-                      ": " + str(model4.wv.similarity(test_word4, test_word5)))
-    outfile.close()
+    print(wv['hello'])
 
 
 if __name__ == "__main__":
